f877f4e2e623c2bafc6b4b74839752a45996d30b
IndigoWizard <microclubist@gmail.com>
2021-01-18 03:51:28 +0100

    Hooking the app with OSM API
    using Geopy and Nominatim to hook the app with OSM API to fetch usere's input address and spawn the map.

--- yoda.py
+++ yoda.py
@@ -1,57 +1,39 @@
 import folium
 import webbrowser
 import time
+import requests
+import geopy
+from geopy.geocoders import Nominatim
 
-# defining variable
-address = []
-locations = ["custom location", "micro club", "tokyo", "yokohama", "algiers", "usthb"]
-answer = ""
 tip = "Baby Yoda on a trip!"
-restart='yes'
-float 
-# intro
-print("Greetings!\nThis is a basic web map app you can use to spawn map in precise locations.\n Take baby Yoda on a trip!")
-while(restart=='yes'):
-# asking for user's input from the displayed destination list
-  while answer not in locations:
-    answer = input("pick a location from the list or choose custom location: {}\n".format(locations, sep = ",") + "\n >>  ")
-    if answer == "tokyo":
-      address = [35.687307, 139.752268]
-      print("You've chosen Tokyo, please wait for the map to be generated.\n")
-    elif answer == "yokohama":
-      address = [35.4437, 139.6380]
-      ("You've chosen Yokohama, please wait for the map to be generated.\n")
-    elif answer == "algiers":
-      address = [36.772509, 3.059135]
-      ("You've chosen Algiers, please wait for the map to be generated.\n")
-    elif answer == "usthb":
-      address = [36.713283, 3.179707]
-      ("You've chosen USTHB, please wait for the map to be generated.\n")
-    elif answer == "micro club":
-      address = [36.715650, 3.185293]
-    elif answer == "custom location":
-      print("Enter place coordinates, \n e.g: Santa Monica would be: \n 34.00983\n -118.4953\n -------------")
-      lgt = float(input("Enter longitude coordinate: "))
-      lat = float(input("Enter latitude coordinate: "))
-      address = [lgt, lat]
-    else:
-      print("we don't cover that area.\n")
+answer = ""
+address = []
+restart = "yes"
 
-# creating the map with user's chose location
-  m = folium.Map(location=address, tiles='CartoDB dark_matter', zoom_start=14)
+print("Greetings!\n This is a basic web app you can use to spawn a map in a precise location.\n Take baby Yoda on a trip!")
 
-# adding a marker in that same location
-  avatar = folium.features.CustomIcon('BabyYodaSprite.gif', icon_size=(64, 64))
-  folium.Marker(location=address, tooltip=tip, icon=avatar).add_to(m)
+while (restart=="yes"):
+	geolocator = Nominatim(user_agent="yoda_app")
+	answer = input("pick location: \n >>  ")
+	location = geolocator.geocode(answer)
 
-#saving the map as an html fiel in the same app folder
-  m.save('map.html')
+	print((location.latitude, location.longitude))
+	address = [location.latitude, location.longitude]
 
-  print("map generated successfuly.")
-# openning the generatd map in default browser
-  time.sleep(1)
-  webbrowser.open_new_tab('map.html')
-  restart = input("wanna go somewhere else? \n")
-  answer=""
-  if not restart == "yes":
-    break
+	m = folium.Map(location=address, tiles='CartoDB dark_matter', zoom_start=14)
+
+	# adding a marker in that same location
+	avatar = folium.features.CustomIcon('BabyYodaSprite.gif', icon_size=(64, 64))
+	folium.Marker(location=address, tooltip=tip, icon=avatar).add_to(m)
+
+	#saving the map as an html fiel in the same app folder
+	m.save('map.html')
+
+	print("map generated successfuly.")
+	# openning the generatd map in default browser
+	time.sleep(1)
+	webbrowser.open_new_tab('index.html')
+	restart = input("wanna go somwhere else?  < yes / no > \n")
+	answer = ""
+	if not restart == "yes":
+		break
